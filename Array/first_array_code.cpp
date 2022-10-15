aee797486fffcabf30262ce78494f22e932a6230
Sejal kathane <kathanesejal@gmail.com>
2022-10-15 11:34:32 +0530

    make array folder with simple code

---
+++ Array/first_array_code.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //we simply insilize array
+    int n=6;
+    int arr[n]={1,2,3,4,5,6};
+
+    //apply for loop to traverse array and try to print it
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+ 
+    return 0;
+}
